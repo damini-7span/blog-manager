4f9ec04ff80a56951587586759ef0d735c51256e
damini-7span <damini@7span.com>
2022-05-27 11:02:40 +0530

    Init Commit

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,92 @@
+<?php return array (
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'laravel/passport' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Passport\\PassportServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'plank/laravel-mediable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Plank\\Mediable\\MediableServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'MediaUploader' => 'Plank\\Mediable\\Facades\\MediaUploader',
+      'ImageManipulator' => 'Plank\\Mediable\\Facades\\ImageManipulator',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+  ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
+  'spatie/laravel-query-builder' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\QueryBuilder\\QueryBuilderServiceProvider',
+    ),
+  ),
+);
---
+++ storage/framework/views/f9ae29abd8388fbce75dfcfc6df3437cc61fc78d.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>7Span</title>
+</head>
+<body>
+    <h1>Forget Password</h1>
+    <p>Code : <?php echo e($data['code']); ?> </p>
+   
+    <p>Thank you</p>
+</body>
+</html><?php /**PATH /opt/lampp/htdocs/blog-manager-api/resources/views/ForgetPassword.blade.php ENDPATH**/ ?>
