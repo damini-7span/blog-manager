c177a8696a24bca4e152b94c782521030a4358f2
damini-7span <damini@7span.com>
2022-06-08 14:22:27 +0530

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\BaseModel;
+use Plank\Mediable\Mediable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use BaseModel, HasApiTokens, HasFactory, Notifiable, HasRoles, Mediable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+}
